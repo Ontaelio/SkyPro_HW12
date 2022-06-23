641d91f8f0a6b7348bd105a3f3d3cc90cd3755ca
Dmitry Reznikov <Ontaelio@users.noreply.github.com>
2022-06-23 17:37:33 +0300

    some docstrings added

--- functions.py
+++ functions.py
@@ -4,6 +4,11 @@ from loggers import file_logger
 
 
 def get_json(fname='posts.json'):
+    """
+    get data from JSON
+    :param fname: JSON filename
+    :return: list of posts
+    """
     try:
         with open(fname, encoding='utf-8') as fin:
             posts = json.load(fin)
@@ -19,6 +24,12 @@ def get_json(fname='posts.json'):
 
 
 def write_json(posts, fname='posts.json'):
+    """
+    save data to JSON
+    :param posts: list of posts
+    :param fname: JSON filename
+    :return:
+    """
     try:
         with open(fname, 'w', encoding='utf-8') as fout:
             json.dump(posts, fout, indent=1, ensure_ascii=False)
@@ -27,6 +38,12 @@ def write_json(posts, fname='posts.json'):
 
 
 def find_posts(posts: list, s: str) -> list:
+    """
+    find posts containing string str
+    :param posts: a list of posts
+    :param s: search string
+    :return:
+    """
     filtered = []
     for post in posts:
         if s in post["content"]: filtered.append(post)
@@ -34,8 +51,12 @@ def find_posts(posts: list, s: str) -> list:
 
 
 def add_post(pic: str, post: str):
+    """
+    add a post to json
+    :param pic: picture link
+    :param post: post text
+    :return:
+    """
     posts = get_json()
     posts.append({'pic': pic, 'content': post})
     write_json(posts)
-
-
